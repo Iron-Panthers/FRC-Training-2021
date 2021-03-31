93ef80726f79bc4ad0b0bdd8698abef5b5b4112c
LandonS <landonl.stobaugh@gmail.com>
2021-03-30 18:28:00 -0700

    Update ExampleCommand.java

--- FRC-Training-2021/src/main/java/frc/robot/commands/ExampleCommand.java
+++ FRC-Training-2021/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -2,6 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ *    Code edited by Landon Stobaugh 3/30/21
+ *      Remember, stay cool, and keep on hacking
+ */
+
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
