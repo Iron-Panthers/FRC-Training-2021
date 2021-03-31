a78fc06df6a85bb1b7c2754f379634b614f43fe5
LandonS <landonl.stobaugh@gmail.com>
2021-03-30 18:09:54 -0700

    cool subsystem update

--- FRC-Training-2021/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ FRC-Training-2021/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -2,6 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ *    Code edited by Landon Stobaugh 3/30/21
+ *      Remember, stay cool, and keep on hacking
+ */
+
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -15,7 +20,7 @@ public class ExampleSubsystem extends SubsystemBase {
   private Spark motor1;
   private TalonFX motor2;
   public ExampleSubsystem() {
-	  motor1 = new Spark(Constants.ExampleSubsystem.MOTOR_1_PORT); // What does 6 mean anyway?
+	  motor1 = new Spark(Constants.ExampleSubsystem.MOTOR_1_PORT); 
 	  motor2 = new TalonFX(Constants.ExampleSubsystem.MOTOR_2_PORT);
   }
 
@@ -27,5 +32,15 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+  
+  public void setMotors(double inp_speed)
+  {
+    // Set the speed of the different motors accordingly
+    motor1.set(inp_speed);
+    
+    // Because the motors are facing opposite directions
+    // it's important to invert the spin direction on the opposing motor
+    motor2.set(-inp_speed);
   }
 }
