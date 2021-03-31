a0524a7ecb34b7995c6a7a460d2bca66388ddb2b
isaac <isaac@rubenfamily.com>
2021-03-30 18:07:10 -0700

    add drivebase?

---
+++ FRC-Training-2021/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivebase extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private Spark motor1;
+  private TalonFX motor2;
+  public Drivebase() {
+	  motor1 = new Spark(Constants.ExampleSubsystem.MOTOR_1_PORT); // What does 6 mean anyway?
+	  motor2 = new TalonFX(Constants.ExampleSubsystem.MOTOR_2_PORT);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
