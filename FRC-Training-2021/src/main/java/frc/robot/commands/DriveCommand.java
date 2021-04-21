e60a805b942d8e9447b52b29b934cb6078ebe272
sarah <23mins22@stu.smuhsd.org>
2021-04-20 18:33:32 -0700

    update subsystem and command

--- FRC-Training-2021/src/main/java/frc/robot/commands/DriveCommand.java
+++ FRC-Training-2021/src/main/java/frc/robot/commands/DriveCommand.java
@@ -5,24 +5,44 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
 
 public class DriveCommand extends CommandBase {
   /** Creates a new DriveCommand. */
-  public DriveCommand() {
+  private final Drive drive;
+  private final double x, y;
+
+  public DriveCommand(double x, double y, Drive drive) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drive);
+
+    this.drive = drive;
+    this.x = x;
+    this.y = y;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // set left + right as x + y 
+    // x = y --> go forward/backward, x > y --> turn right, x < y --> turn left
+    double leftSpeed = x;
+    double rightSpeed = y;
+
+    drive.setPower(0.5, 0.5);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.setPower(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
--- FRC-Training-2021/src/main/java/frc/robot/subsystems/Drive.java
+++ FRC-Training-2021/src/main/java/frc/robot/subsystems/Drive.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,13 +12,21 @@ import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
   /** Creates a new Drive. */
-    private WPI_TalonFX left_follow = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_1_FOLLOW);
-    private WPI_TalonFX left_leader = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_1_LEADER);
-    private WPI_TalonFX right_leader = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_2_LEADER);
-    private WPI_TalonFX right_follow = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_2_FOLLOW);
+  private WPI_TalonFX left_follow = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_1_FOLLOW);
+  private WPI_TalonFX left_leader = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_1_LEADER);
+  private WPI_TalonFX right_leader = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_2_LEADER);
+  private WPI_TalonFX right_follow = new WPI_TalonFX(Constants.DriveSubsystem.MOTOR_2_FOLLOW);
+
   public Drive() {
+    // make follower motors follow leader motors
     left_leader.follow(left_follow);
     right_leader.follow(right_follow);
+  }
+
+  // set motor power
+  public void setPower(double leftPower, double rightPower) {
+    left_leader.set(TalonFXControlMode.PercentOutput, leftPower);
+    right_leader.set(TalonFXControlMode.PercentOutput, rightPower);
   }
 
   @Override
